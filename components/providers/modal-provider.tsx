37b390a687d570367edaa1af7834453ad554b2ad
Kaushik Sharma <kaushik445566@gmail.com>
2023-09-15 18:42:35 +0530

    feat: edit members

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -3,9 +3,9 @@
 import { useEffect, useState } from "react";
 
 import { EditServerModal } from "@/components/modals/edit-server-modal";
-
 import { InviteModal } from "@/components/modals/invite-modal";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
+import { MembersModal } from "@/components/modals/members-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -23,6 +23,7 @@ export const ModalProvider = () => {
       <CreateServerModal />
       <InviteModal />
       <EditServerModal />
+      <MembersModal />
     </>
   );
 };
